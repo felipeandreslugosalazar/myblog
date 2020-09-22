7251a4c0dea3058d84ce97d5adf0ff06fbe14a0e
Felipe Andres Lugo Salazar <felipeandreslugo@gmail.com>
2020-09-22 22:24:11 +0200

    react front end first part ready

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from './logo.svg';
+import "./App.css";
+import AboutPage from "./pages/AboutPage.js";
+import ArticlePage from "./pages/ArticlePage.js";
+import ArticlesListPage from "./pages/ArticlesListPage.js";
+import Homepage from "./pages/HomePage.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from "./NavBar.js";
+import NotFoundPage from "./pages/NotFoundPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <Homepage />
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <NavBar />
+          <div id="page-body">
+            <Switch>
+              <Route path="/" component={Homepage} exact />
+              <Route path="/about" component={AboutPage} />
+              <Route path="/articles-list" component={ArticlesListPage} />
+              <Route path="/article/:name" component={ArticlePage} />
+              <Route component={NotFoundPage} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const HomePage = () => (
+  <>
+    <h1>Hello, welcome to my Blog</h1>
+    <p>
+      Minim dolor nulla commodo officia voluptate nulla voluptate proident id.
+      Minim aute ullamco ea sint. Ut proident ex anim eu. Non deserunt minim
+      consectetur laborum cupidatat elit est enim voluptate minim qui ex laboris
+      non. Laboris laborum sit ipsum veniam irure voluptate deserunt quis duis
+      deserunt id veniam eu. Lorem exercitation fugiat voluptate consequat nisi
+      amet. Elit esse cupidatat enim eiusmod et reprehenderit duis nisi culpa
+      officia amet adipisicing ea qui. Dolor irure laborum sunt consectetur
+      officia.
+    </p>
+    <p>
+      Mollit quis excepteur non ut cillum consequat eiusmod dolor. Ex enim
+      aliquip deserunt excepteur sint laboris excepteur irure commodo consequat
+      minim. Voluptate eu mollit non reprehenderit Lorem aliquip ad cillum
+      tempor quis voluptate eiusmod. Laboris et et velit laborum. Consectetur
+      esse velit pariatur proident id laboris. Quis nisi adipisicing est
+      consequat sit proident cillum veniam in sit ad amet esse duis. Minim
+      fugiat cillum laborum nostrud magna excepteur fugiat ut est veniam
+      reprehenderit consectetur elit. Magna occaecat adipisicing eiusmod ullamco
+      elit consectetur voluptate Lorem occaecat aliquip occaecat.
+    </p>
+  </>
+);
+
+export default HomePage;
