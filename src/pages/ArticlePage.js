07703af366104b7a02528b15d579f3b2c4eb9b77
Felipe Andres Lugo Salazar <felipeandreslugo@gmail.com>
2020-09-27 19:38:59 +0200

    displaying comments

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -2,6 +2,8 @@
 import React, { useState, useEffect } from "react";
 import articleContent from "./article-content";
 import ArticlesList from "../components/ArticlesList";
+import CommentsList from "../components/CommentsList";
+
 import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = ({ match }) => {
@@ -39,6 +41,7 @@ const ArticlePage = ({ match }) => {
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
+      <CommentsList comments={articleInfo.comments} />
       <h3>Other Articles:</h3>
       <ArticlesList articles={otherArticles} />
     </>
